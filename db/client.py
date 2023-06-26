50b62afd6e3235a8a6c1a721b82b1f133192541f
l0ngnguyen <long.nguyen6@gmail.com>
2023-06-26 22:41:45 +0700

    tái cấu trúc project

---
+++ db/client.py
@@ -0,0 +1,20 @@
+import ...
+
+# Định nghĩa một class gồm các method để connect và thao tác với database
+
+class Client:
+    def __init__(self, connection_string):
+        self.client = "code ở đây"
+
+    def query_sql(self, sql_query):
+        pass
+
+    def update(self, ....):
+        pass
+
+    def delete(self, ....):
+        pass
+
+    def is_exist_record(self, id):
+        # kiểm tra xem bài báo có id như này đã tồn tại chưa
+        pass
---
+++ db/data.py
@@ -0,0 +1,15 @@
+import ...
+
+# Tất cả những gì liên quan đến lấy dữ liệu từ database thì viết funciton ở đây
+
+def get_news_by_id(cl, news_id):
+    client.query_sql(..., news_id)
+    return result
+
+
+def load_all_data(cl, )
+    client.
+
+
+def get_news_in_range_date(cl, from_date, to_date):
+    pass
